d9e84a32615b36d331158ba3b189762006772985
Viraj Dicholkar <dicholkar.vv@gmail.com>
2020-10-24 12:18:13 +0530

    added functionality to add new box

--- src/app/movable-box/box-container/box-container.component.ts
+++ src/app/movable-box/box-container/box-container.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Box {
+  style: any;
+  boxNumber: number;
+}
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'box-container',
@@ -7,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box-container.component.scss']
 })
 export class BoxContainerComponent implements OnInit {
-
   constructor() { }
+
+  boxList: Array<Box> = [];
 
   ngOnInit(): void {
   }
+
+  addNewBox(): void {
+    const listLength = this.boxList.length;
+    const boxNumber = listLength + 1;
+    const zIndex = listLength ? this.boxList[listLength - 1].style['z-index'] + 1 : 1;
+
+    const newBox: Box = {
+      style: {
+        'z-index': zIndex
+      },
+      boxNumber
+    };
+    console.log('newBox', newBox)
+    this.boxList.push(newBox);
+  }
+
 
 }

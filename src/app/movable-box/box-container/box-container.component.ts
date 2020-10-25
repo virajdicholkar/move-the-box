f76008406c221561186ab76945ecea60f3de32eb
Viraj Dicholkar <dicholkar.vv@gmail.com>
2020-10-25 11:21:51 +0530

    added limits to box movement and functionality to reset active box position

--- src/app/movable-box/box-container/box-container.component.ts
+++ src/app/movable-box/box-container/box-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 interface Box {
   style: any;
@@ -10,13 +10,23 @@ interface Box {
   templateUrl: './box-container.component.html',
   styleUrls: ['./box-container.component.scss']
 })
-export class BoxContainerComponent implements OnInit {
+export class BoxContainerComponent implements OnInit, AfterViewInit {
   constructor() { }
   activeBox: Box;
   boxList: Array<Box> = [];
   readonly speedInPx = 10;
-
+  movableScreenWidth = 0;
+  movableScreenHeight = 0;
+  boxWidth = 100;
+  boxHeight = 50;
+  @ViewChild('screen') screen: ElementRef;
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.movableScreenWidth = this.screen.nativeElement.offsetWidth - this.boxWidth - 2;
+    this.movableScreenHeight = this.screen.nativeElement.offsetHeight - this.boxHeight - 2;
   }
 
   addNewBox(): void {
@@ -27,7 +37,9 @@ export class BoxContainerComponent implements OnInit {
 
     const newBox: Box = {
       style: {
-        'z-index': zIndex
+        'z-index': zIndex,
+        'height.px': this.boxHeight,
+        'width.px': this.boxWidth
       },
       boxNumber
     };
@@ -47,6 +59,7 @@ export class BoxContainerComponent implements OnInit {
 
   @HostListener('window:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
+    console.log('event', event)
     if (!this.activeBox) {
       return;
     }
@@ -71,6 +84,13 @@ export class BoxContainerComponent implements OnInit {
         this.moveBoxRight();
         break;
 
+      case 'r':
+        this.resetActiveBoxPosition();
+        break;
+
+      case 'Delete':
+        this.deleteActiveBox();
+        break;
       default:
         return;
     }
@@ -78,21 +98,41 @@ export class BoxContainerComponent implements OnInit {
 
   moveBoxUp(): void {
     const marginTop = this.activeBox.style['margin-top.px'] || 0;
-    this.activeBox.style = { ...this.activeBox.style, 'margin-top.px': marginTop - this.speedInPx };
+    this.activeBox.style = { ...this.activeBox.style, 'margin-top.px': this.getNewPosition(marginTop, -this.speedInPx) };
+    console.log('marginTop after', marginTop - this.speedInPx)
   }
 
   moveBoxDown(): void {
     const marginTop = this.activeBox.style['margin-top.px'] || 0;
-    this.activeBox.style = { ...this.activeBox.style, 'margin-top.px': marginTop + this.speedInPx };
+    this.activeBox.style = { ...this.activeBox.style, 'margin-top.px': this.getNewPosition(marginTop) };
   }
+
 
   moveBoxLeft(): void {
     const marginLeft = this.activeBox.style['margin-left.px'] || 0;
-    this.activeBox.style = { ...this.activeBox.style, 'margin-left.px': marginLeft - this.speedInPx };
+    this.activeBox.style = { ...this.activeBox.style, 'margin-left.px': this.getNewPosition(marginLeft, -this.speedInPx, true) };
   }
 
   moveBoxRight(): void {
     const marginLeft = this.activeBox.style['margin-left.px'] || 0;
-    this.activeBox.style = { ...this.activeBox.style, 'margin-left.px': marginLeft + this.speedInPx };
+    this.activeBox.style = { ...this.activeBox.style, 'margin-left.px': this.getNewPosition(marginLeft, this.speedInPx, true) };
+  }
+
+  resetActiveBoxPosition(): void {
+    this.activeBox.style = {
+      ...this.activeBox.style,
+      'margin-left.px': 0,
+      'margin-top.px': 0
+    };
+
+  }
+
+  getNewPosition(currentMargin, speed = this.speedInPx, horizontal = false): number {
+    const limit = horizontal ? this.movableScreenWidth : this.movableScreenHeight;
+    const newPosition = currentMargin + speed;
+    if (limit > Math.abs(newPosition)) {
+      return newPosition;
+    }
+    return Math.sign(speed) * limit;
   }
 }
